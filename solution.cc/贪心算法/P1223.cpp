ab32beb90d3e88617b23d039598541fac9f7e9e4
zhou <zhoubye@foxmail.com>
2025-01-10 14:16:35 +0800

    new :: cc.so -> [贪心算法][P1223 排队接水]

---
+++ solution.cc/贪心算法/P1223.cpp
@@ -0,0 +1,43 @@
+// == Code Location ==
+#include <algorithm>
+#include <cstdio>
+#include <vector>
+
+template <typename T> using vec = std::vector<T>;
+
+struct data {
+  int k;
+  int t;
+};
+
+bool compare(data &a, data &b) { return a.t < b.t; }
+
+void solve(void) {
+  int n;
+  scanf("%d", &n);
+
+  vec<data> a(n);
+  for (int i = 0; i < n; i++) {
+    a[i].k = i + 1;
+    scanf("%d", &a[i].t);
+  }
+
+  std::sort(a.begin(), a.end(), compare);
+
+  int tot = 0, i = 0;
+  for (auto [k, t] : a) {
+    printf("%d ", k);
+    tot += t * (n - 1 - i);
+    i += 1;
+  }
+
+  double ans = tot * 1.0 / n;
+  printf("\n%.2lf\n", ans);
+}
+
+int main() {
+  freopen("data.in", "r", stdin);
+  solve();
+  fclose(stdin);
+  return 0;
+}
